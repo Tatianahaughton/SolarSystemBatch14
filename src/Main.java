d88aa823bb4784a79702c50e431b1911cb46649c
tatianahaughton <tatianahaughton33@gmail.com>
2022-12-18 11:20:10 -0500

    The basic project Solar System is ready for review

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Basic structure of project");
+
+        SolarSystem Jinny=new SolarSystem();
+        Jinny.Sun=1;
+        Jinny.Moon=2;
+        Jinny.Planets=3;
     }
 }
